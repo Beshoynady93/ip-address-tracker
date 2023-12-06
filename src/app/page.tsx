5dc2a81aff385150abe9bf777033514cccae31bb
Beshoynady93 <101524188+Beshoynady93@users.noreply.github.com>
2023-12-06 21:23:10 +0200

    Leaflet and geoipify api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <main className=""></main>;
+import dynamic from 'next/dynamic';
+
+const apiKey = 'at_i5h7K8tcdAwCZO7YVsfTloFhnzXcG';
+
+const Map = dynamic(() => import('./components/Leaflet'), {
+  ssr: false,
+});
+
+export default async function Home() {
+  const data = await fetch(
+    `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((location) => location);
+
+  const position = [data.location.lat, data.location.lng];
+
+  console.log(data);
+
+  return <main className="">{<Map position={position} zoom={13} />}</main>;
 }
